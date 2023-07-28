32de057fa9a9aacd4d669525f2163599efb79207
Mayaess <140650784+Mayaess@users.noreply.github.com>
2023-07-28 19:38:00 +0300

    Add files via upload

--- Logika/m2/u1/VSC. Класи/main.py
+++ Logika/m2/u1/VSC. Класи/main.py
@@ -1,1 +1,18 @@
-print("Hello, World")
+class Buttonz():
+    def __init__(self, title_text, x_num, y_num):
+        self.title = title_text
+        self.x = x_num
+        self.y = y_num
+        self.appearance = True
+    def hide(self):
+        self.appearance = False
+    def show(self):
+        self.appearance = True
+    def print_status(self):
+        print('Дані про віджет:')
+        print(self.title, self.x, self.y, self.appearance)
+
+ok_button = Buttonz('ok', 100, 100)
+ok_button.print_status()
+ok_button.hide()
+ok_button.print_status()
