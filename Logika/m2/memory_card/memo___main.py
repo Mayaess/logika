7b1544b4fc243ee9b72355d0f0ea34ad280f8893
Mayaess <140650784+Mayaess@users.noreply.github.com>
2023-08-25 19:38:47 +0300

    Add files via upload

--- Logika/m2/memory_card/memo___main.py
+++ Logika/m2/memory_card/memo___main.py
@@ -1,8 +1,14 @@
 from memo___card_layout import *
 from PyQt5.QtWidgets import QWidget
 from random import shuffle # будемо змішувати відповіді в картці питання
-
+from memo__data import*
 card_width, card_height = 600, 500 # початкові розміри вікна "картка"
+
+radio_list = [radio_btn1, radio_btn2, radio_btn3, radio_btn4]
+
+frm = Question('vd', "fdb", 'vfdgbgfb', 'bddbgdb', 'afvd')
+
+frm_card = QuestionView(frm, lb_question, radio_list[0], radio_list[1], radio_list[2], radio_list[3])
 
 def show_data():
     ''' показує на екрані потрібну інформацію '''
@@ -17,7 +23,7 @@ def check_result():
 win_card = QWidget()
 win_card.resize(card_width, card_height)
  #тут повинні бути параметри вікна
-
+frm_card.show()
 win_card.setLayout(layout_card) 
 win_card.show()
 app.exec_()
