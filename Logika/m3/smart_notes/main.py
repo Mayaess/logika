fd3339f3243ca6c6d3f5ba9455dac3b4059f6109
Mayaess <140650784+Mayaess@users.noreply.github.com>
2023-10-03 19:21:14 +0300

    Add files via upload

--- Logika/m3/smart_notes/main.py
+++ Logika/m3/smart_notes/main.py
@@ -7,6 +7,10 @@ from PyQt5.QtWidgets import (
 
 import json
 
+def saveToFile():
+    with open('notes.json', 'w', encoding='utf8') as file:
+        json.dump(notes, file, ensure_ascii=False, sort_keys=True, indent=4)
+
 app = QApplication([])
 window = QWidget()
 
@@ -24,7 +28,7 @@ lb_tags = QLabel('Список тегів')
 
 lst_tags = QListWidget()
 
-field_ефп = QTextEdit()
+field_tag = QLineEdit()
 btn_tag_add = QPushButton('Додати тег')
 btn_tag_unpin = QPushButton('Видалити тег')
 btn_tag_search = QPushButton('Шукати замітки за тегом')
@@ -48,11 +52,54 @@ row1.addWidget(btn_note_delete)
 col2.addLayout(row1)
 col2.addWidget(btn_note_save)
 
-def show_notes():
-    key = lst_notes.selectedItems()[0].text()
+col2.addWidget(lb_tags)
+col2.addWidget(lst_tags)
+
+col2.addWidget(field_tag)
+
+row2 = QHBoxLayout()
+row2.addWidget(btn_tag_add)
+row2.addWidget(btn_tag_unpin)
+
+col2.addLayout(row2)
+col2.addWidget(btn_tag_search)
+
+
+def show_note():
+    key = lst_notes.currentItem().text()
     field_text.setText(notes[key]['текст'])
 
-lst_notes.itemClicked.connect(show_notes)
+    lst_tags.clear()
+    lst_tags.addItems(notes[key]['теги'])
+
+def save_note():
+    key = lst_notes.currentItem().text()
+    notes[key]['текст'] = field_text.toPlainText()
+
+    saveToFile()
+
+def add_note():
+    note_name, ok = QInputDialog.getText(window, "Створювання замітки", "Назва замітки")
+
+    if note_name and ok:
+        notes[note_name] = {'текст': '  ', "теги": []}
+        lst_notes.addItem(note_name)
+
+
+def delete_note():
+    key = lst_notes.currentItem().text()
+    del notes[key]
+    saveToFile()
+
+    field_text.clear()
+    lst_tags.clear()
+    lst_notes.clear()
+    lst_notes.addItems(notes)
+
+lst_notes.itemClicked.connect(show_note)
+btn_note_save.clicked.connect(save_note)
+btn_note_delete.clicked.connect(delete_note)
+btn_note_create.clicked.connect(add_note)
 
 with open('notes.json', 'r', encoding='utf8') as file:
     notes = json.load(file)
